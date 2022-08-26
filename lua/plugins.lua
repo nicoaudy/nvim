3455ce8e55a90b91d4f0829d519c0ef622d8ad92
Nico Audy <nicoaudy1994@gmail.com>
2022-08-26 13:52:16 +0700

    update packer sync

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,45 @@
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+local fn = vim.fn
+
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+-- Automatically install packer
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  require('packer').packadd = 'packer.nvim'
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+local group = vim.api.nvim_create_augroup("packer_user_config", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  command = "source <afile> | PackerSync",
+  pattern = "plugins.lua",
+  group = group,
+})
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
   use "terryma/vim-multiple-cursors"                          -- Multicursor <C-n>
   use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
@@ -42,4 +82,11 @@ return require('packer').startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use "lukas-reineke/indent-blankline.nvim"
+  
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
+
