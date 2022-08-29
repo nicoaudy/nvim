f2488ac0ba783317affa142827b3712ff0ea0437
Nico Audy <nicoaudy1994@gmail.com>
2022-08-29 14:29:32 +0700

    null-ls not working on 0.8

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -2,15 +2,27 @@ local null_ls_status_ok, null_ls = pcall(require, "null_ls")
 if not null_ls_status_ok then
   return
 end
+
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
-
 
 null_ls.setup({
   debug = false,
   sources = {
-    formatting.prettier,
+    formatting.prettier.with({
+      filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "jsonc", "yaml", "markdown", "graphql", "handlebars", "svelte" },
+      only_local = "node_modules/.bin",
+    }),
     formatting.black.with({ extra_args = { "--fast" } }),
     formatting.stylua,
   },
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+      vim.api.nvim_create_autocmd(
+        "BufWritePre",
+        { buffer = bufnr, callback = vim.lsp.buf.format({ async = true }), group = group }
+      )
+    end
+  end,
 })
